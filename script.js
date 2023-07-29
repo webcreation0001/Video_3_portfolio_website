adb1187a1223c9954afad7fb1cd7fdd08af6f8be
webcreation0001 <137776530+webcreation0001@users.noreply.github.com>
2023-07-29 19:34:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+
+window.onscroll = () =>{
+    let header =document.querySelector('header');
+    header.classList.toggle('sticky',window.scrollY>100);
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
